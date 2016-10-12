52d8c9063eca0ec60c3bfa0a0750e7577f342003
f-zhao <850707492@qq.com>
2016-10-12 19:00:04 +0800

    建造者模式

---
+++ src/com/zhaofeng/builder/Builder.java
@@ -0,0 +1,11 @@
+package com.zhaofeng.builder;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 赵峰
+ * Date: 2016/10/12
+ * Time: 17:39
+ * To change this template use File | Settings | File Templates.
+ */
+public class Builder {
+}
