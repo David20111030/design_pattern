0771445d51cc04aa7f30472f71d2ca730cfb6b68
f-zhao <850707492@qq.com>
2016-10-26 20:04:03 +0800

    桥接模式

---
+++ src/com/zhaofeng/bridge/Client.java
@@ -0,0 +1,11 @@
+package com.zhaofeng.bridge;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 赵峰
+ * Date: 2016/10/26
+ * Time: 15:05
+ * To change this template use File | Settings | File Templates.
+ */
+public class Client {
+}
