a044fc644e70107451a19f6a7d15b23bae9ab20b
f-zhao <850707492@qq.com>
2016-10-31 09:59:48 +0800

    装饰模式

---
+++ src/com/zhaofeng/decorator/ConcreteComponent.java
@@ -0,0 +1,11 @@
+package com.zhaofeng.decorator;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 赵峰
+ * Date: 2016/10/31
+ * Time: 9:28
+ * To change this template use File | Settings | File Templates.
+ */
+public class ConcreteComponent {
+}
---
+++ src/com/zhaofeng/decorator/ConcreteDecoratorA.java
@@ -0,0 +1,11 @@
+package com.zhaofeng.decorator;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 赵峰
+ * Date: 2016/10/31
+ * Time: 9:30
+ * To change this template use File | Settings | File Templates.
+ */
+public class ConcreteDecoratorA {
+}
