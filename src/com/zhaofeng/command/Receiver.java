c121c08ea04e43543cd1e12c1198191977382f02
f-zhao <850707492@qq.com>
2016-11-02 17:23:07 +0800

    命令模式

---
+++ src/com/zhaofeng/command/Receiver.java
@@ -0,0 +1,11 @@
+package com.zhaofeng.command;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 赵峰
+ * Date: 2016/11/2
+ * Time: 17:21
+ * To change this template use File | Settings | File Templates.
+ */
+public class Receiver {
+}
