aaf1510e943c1c23ca5121c4dda80754234b7838
f-zhao <850707492@qq.com>
2016-11-02 17:20:03 +0800

    门面模式

---
+++ src/com/zhaofeng/facade/SystemA.java
@@ -0,0 +1,11 @@
+package com.zhaofeng.facade;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 赵峰
+ * Date: 2016/11/2
+ * Time: 17:18
+ * To change this template use File | Settings | File Templates.
+ */
+public class SystemA {
+}
